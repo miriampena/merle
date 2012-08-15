73950f4fb30c07cb9474967ce98888950493bb49
jesse-ad <jesse@adroll.com>
2012-08-15 16:19:55 -0700

    Fixed dynamic merle compile

--- src/merle_cluster.erl
+++ src/merle_cluster.erl
@@ -11,14 +11,18 @@ configure(MemcachedHosts, ConnectionsPerHost) ->
     DynModuleBegin = "-module(merle_cluster_dynamic).
         -export([get_server/1]).
         get_server(ClusterKey) -> N = erlang:phash2(ClusterKey, ~p), 
-            do_get_server(N).",
+            do_get_server(N).\n",
     DynModuleMap = "do_get_server(~p) -> {\"~s\", ~p}; ",
     DynModuleEnd = "do_get_server(_N) -> throw({invalid_server_slot, _N}).\n",
+    
     ModuleString = lists:flatten([
         io_lib:format(DynModuleBegin, [length(SortedMemcachedHosts)]),
         index_map(fun([Host, Port], I) -> io_lib:format(DynModuleMap, [I-1, Host, Port]) end, SortedMemcachedHosts),
         DynModuleEnd
     ]),
+    
+    log4erl:error("dyn module str ~p", [ModuleString]),
+    
     {M, B} = dynamic_compile:from_string(ModuleString),
     code:load_binary(M, "", B),
 
