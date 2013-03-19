33f474f6bbc8e539b26867d36da718b4764a9a24
jesse-ad <jesse@adroll.com>
2013-03-18 18:07:40 -0700

    Adding debugging logging

--- src/merle_pool.erl
+++ src/merle_pool.erl
@@ -129,6 +129,9 @@ handle_call({join, Name, Index, Pid}, _From, S = #server_state{pools = Pools}) -
         false ->
             {reply, no_such_group, S};
         true ->
+            %TODO: delete this
+            log4erl:error("Client is joining pool ~p at index ~p", [Name, Index]),
+
             % insert new pid into the table
             ets:insert(?PIDS_TABLE, {{Name, Index}, Pid}),
 
